ca18379c973a9316a2080ba18b1303e7eb3edc5c
Rafno <rafnar1993@gmail.com>
2018-02-03 23:39:25 +0000

    Final commit.

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const express = require('express')
-  , app = express()
-  , articles = require('./articles')
-  , path = require('path')
-  , md = require('markdown-it')();
-app.all('/*.md', function (req, res) {
+const express = require('express');
+const articles = require('./articles');
+const path = require('path');
+const md = require('markdown-it')();
+
+const app = express();
+/**
+ * Kemur í veg fyrir að það sé hægt að sjá .md skjölin.
+ * teiknar 403 villu ef aðili reynir að skoða.
+*/
+app.all('/*.md', (req, res) => {
   res.render('index', {
     title: '403',
     h1: 'Meinaður aðgangur',
     p: md.render('# Þú hefur ekki aðgang að .md skjölum'),
-    a: '<a href=\' / \'>Til baka</a>'
+    a: '<a href=\' / \'>Til baka</a>',
   });
 });
-
+/**
+ * gefur aðgang að skjölum/myndum
+ */
 const hostname = '127.0.0.1';
 const port = 3000;
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
@@ -25,5 +30,3 @@ app.use(articles);
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
-
-
--- articles.js
+++ articles.js
@@ -1,42 +1,69 @@
-/* útfæra greina virkni */
-const fs = require('fs')
-  , fm = require('front-matter')
-  , md = require('markdown-it')()
-  , util = require('util')
-  , express = require('express')
-  , router = express.Router();
+const fs = require('fs');
+const fm = require('front-matter');
+const md = require('markdown-it')();
+const util = require('util');
+const express = require('express');
 
+/**
+ * Global breytur
+ */
+const router = express.Router();
+const articles = './articles/';
+
+// **************FILES********************//
+/**
+ * Async les inn skjöl eftir að directory les inn ./articles
+ */
 const readFileAsync = util.promisify(fs.readFile);
-
+/**
+ * @param {any} file
+ * Tekur inn hvaða skjöl sem er, skilar þeim í strengjaformi.
+ */
 function read(file) {
   return readFileAsync(file)
     .then(data => data.toString());
 }
+/**
+ * readFiles tekur inn skjöl og ýtir þeim í read(),
+ * eftir að skjalið er búið að lesa, setur það í fylki og skilar því.
+ * @param {any} files
+ * @returns {object} result
+ */
 async function readFiles(files) {
   const result = [];
-
-  for (let i = 0; i < files.length; i++) {
-    result.push(await read(files[i]));
+  // Hver hafði hugmyndina að láta eslint banna ++?
+  for (let i = 0; i < files.length; i += 1) {
+    result.push(read(files[i]));
   }
 
   return Promise.all(result);
 }
-//***************DIRECTORY******************//
+// ***************DIRECTORY******************//
 const readDirAsync = util.promisify(fs.readdir);
-articles = './articles/';
-
+/**
+ * Les inn aðeins þau skjöl í directory sem enda á .md, hunsar þannig með img skjalið.
+ * @param {any} directory
+ * @returns {string} files
+ */
 function readDir(directory) {
   let results = [];
   return readDirAsync(directory)
-    .then(data => {
+    .then((data) => {
       results = data.filter(file => file.substring(file.length - 3) === '.md');
-      for (let i = 0; i < results.length; i++) {
+      for (let i = 0; i < results.length; i += 1) {
         results[i] = articles.concat(results[i]);
       }
       return results;
     });
 }
-
+/**
+ * Slug er slóðin á vefsíðunni, fallið byrjar á að búa til 404 síðu en ef
+ * sluggið matchar við skjölin sem við erum að nota þá tekur það frekar
+ * data úr þeim og teiknar það í staðinn.
+ * @param {any} slug
+ * @param {any} file
+ * @returns {object} rétta slóð/404 slóð.
+ */
 function slugFinder(slug, file) {
   let answer = {
     attributes:
@@ -45,89 +72,115 @@ function slugFinder(slug, file) {
       slug: '404',
     },
     body: '# Error 404 not found',
-  }
-  for (let i = 0; i < file.length; i++) {
+  };
+  for (let i = 0; i < file.length; i += 1) {
     if (file[i].attributes.slug === slug) {
       answer = file[i];
-
     }
   }
   return answer;
 }
-
+/**
+ * Router EF slug er notað.
+ * Main fall.
+ * Byrjar á að lesa directory, senda það í files, lesa skjölin.
+ * Ef allt gengur upp, hendir skjölunum í frontmatter,
+ * Þá er hægt að skoða hvort sluggið passar við skjölin okkar.
+ * Ef svo, þá eru upplýsingunum í skjalinu breytt í markdown og send í render.
+ * Býr til error 500 ef eitthvað fer virkilega úrskeiðis.
+*/
 router.get('/:slug', (req, res) => {
-  const slug = req.params.slug;
-  const filename = readDir(articles).then(filename => {
-    const files = readFiles(filename)
-      .then(data => {
-        const mapped = data.map(file => file = fm(file));
+  const { slug } = req.params;
+  readDir(articles).then((filename) => {
+    readFiles(filename)
+      .then((data) => {
+        const mapped = data.map(file => fm(file));
         const map = slugFinder(slug, mapped);
         const markdown = md.render(map.body);
         res.render('index', {
           title: `${map.attributes.title}`,
           h1: `${map.attributes.title}`,
-          p: `${markdown}`,
-          a: '<a href=\' / \'>Til baka</a>'
+          p: `<p> ${markdown}</p>`,
+          a: '<a href=\' / \'>Til baka</a>',
         });
       })
-      .catch(error => {
-        res.render('index', {
-          title: 'Error 500',
-          h1: 'Villa kom upp',
-          p: '',
-          a: '<a href=\' / \'>Til baka</a>'
-        });
-      });
-  });
-
-});
-/*
-<div class="grid-mid">
-            <div class="row">
-                <div class="col col-6 col-sm-12">
-                  <p>
-                  </p>
-                  </div>
-               <div class="col col-6 col-sm-12">
-                 <p>
-                 </p>
-                 </div>
-              </div>
-*/
-
-function readList(filename) {
-  let sorted = [];
-  let markdown = '';
-  filename.forEach(file => {
-    sorted.push(file.attributes);
-  });
-  sorted.sort(function (a, b) {
-    return Date.parse(a.date) < Date.parse(b.date);
-  });
-  //Work here.
-}
-
-
-router.get('/', (req, res) => {
-  const filename = readDir(articles).then(filename => {
-    const files = readFiles(filename)
-      .then(data => {
-        const mapped = data.map(file => file = fm(file));
-        const sorted = readList(mapped);
-        console.log(sorted);
-        res.render('index', {
-          title: `Gagnasafn`,
-          h1: `Gagnasafnið`,
-          p: '',
-          a: '<a href=\' / \'>Til baka</a>'
-        });
-      })
-      .catch(error => {
+      .catch((error) => {
         res.render('index', {
           title: 'Error 500',
           h1: 'Villa kom upp',
           p: error,
-          a: '<a href=\' / \'>Til baka</a>'
+          a: '<a href=\' / \'>Til baka</a>',
+        });
+      });
+  });
+});
+/**
+ * Teiknar upp indexið. Tekur inn lesin skjöl og raðar þeim eftir dagsetningu.
+ * Byrjar að teikna kassann(row) utan um allt, hoppar svo inn í nokkrar for each
+ * til að búta niður verkefnið.
+ * @param {any} filename
+ * @returns {string} langur html kóði.
+ */
+function readList(filename) {
+  const sorted = [];
+  let image;
+  let row = '<div class="row">';
+  let date;
+  // Þurfum ekki body/textann, tökum það bara út.
+  filename.forEach((file) => {
+    sorted.push(file.attributes);
+  });
+  // sorterað eftir dagsetningu.
+  sorted.sort((a, b) => Date.parse(a.date) < Date.parse(b.date));
+  sorted.forEach((file) => {
+    date = new Date(file.date);
+    date = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+    if (typeof (file.image) === 'undefined') {
+      image = '<div class="photo"></div>';
+    } else {
+      image = `<img src =${file.image}>`;
+    }
+    // Breytan heitir row en í rauninni er hvert svona 'item'. 4 í heild sinni.
+    row +=
+      `<a class="item"
+      href=" ${file.slug}"
+      <div class="pic">
+        ${image}
+      <div class="text">
+      <h1>
+          ${file.title}
+      </h1>
+      <p>
+       ${date} 
+      </p>
+      </div>
+      </a>`;
+  });
+  row += '</div>';
+  return row;
+}
+/**
+* Router fyrir heimasvæði, kallar á readList sem teiknar indexið.
+*/
+router.get('/', (req, res) => {
+  readDir(articles).then((filename) => {
+    readFiles(filename)
+      .then((data) => {
+        const mapped = data.map(file => fm(file));
+        const sorted = readList(mapped);
+        res.render('index', {
+          title: 'Gagnasafn',
+          h1: 'Gagnasafnið',
+          p: sorted,
+          a: '',
+        });
+      })
+      .catch((error) => {
+        res.render('index', {
+          title: 'Error 500',
+          h1: 'Villa kom upp',
+          p: `<p> ${error}</p>`,
+          a: '<a href=\' / \'>Til baka</a>',
         });
       });
   });
