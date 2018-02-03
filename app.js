80dbd7495cf656ad207bcc7c1f4a19a6a347e64b
Rafno <rafnar1993@gmail.com>
2018-02-03 15:50:06 +0000

    Allt shittið

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const app = express();
-const articles = require('./articles');
-const path = require('path');
+const express = require('express')
+  , app = express()
+  , articles = require('./articles')
+  , path = require('path')
+  , md = require('markdown-it')();
+app.all('/*.md', function (req, res) {
+  res.render('index', {
+    title: '403',
+    h1: 'Meinaður aðgangur',
+    p: md.render('# Þú hefur ekki aðgang að .md skjölum'),
+    a: '<a href=\' / \'>Til baka</a>'
+  });
+});
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,8 +19,11 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'articles')));
 app.set('view engine', 'ejs');
 app.use(articles);
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
+
+
